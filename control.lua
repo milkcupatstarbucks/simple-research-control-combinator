5b82bd335f71a3858ef4193f9fcdf3be4fee8144
milkcupatstarbucks <83178133+milkcupatstarbucks@users.noreply.github.com>
2025-07-31 21:22:01 -0700

    Add files via upload

---
+++ control.lua
@@ -0,0 +1,98 @@
+local research_signals = require("research-signals")
+
+function update_research(e)
+    if storage.research_controllers == nil then
+        return
+    end
+    for id, entity in pairs(storage.research_controllers) do
+        if not(entity and entity.valid) then
+            goto continue
+        end
+        local behavior = entity.get_or_create_control_behavior()
+        if behavior == nil then
+            goto continue
+        end
+        local signals_red = behavior.get_circuit_network(defines.wire_connector_id.circuit_red)
+        local signals_green = behavior.get_circuit_network(defines.wire_connector_id.circuit_green)
+        local max = 0
+        local max_tech_name = nil
+        for tech, _ in pairs(research_signals.signal_name_registry) do
+            local sig = 0
+            if signals_red then
+                sig = sig + signals_red.get_signal({type="virtual", name=tech})
+            end
+            if signals_green then
+                sig = sig + signals_green.get_signal({type="virtual", name=tech})
+            end
+            if sig > max then
+                max = sig
+                max_tech_name = tech
+            end
+        end
+        if max_tech_name then
+            local did_something = false
+            while entity.force.current_research and entity.force.current_research.name ~= max_tech_name do
+                entity.force.cancel_current_research()
+                did_something = true
+            end
+            if entity.force.current_research == nil then
+                entity.force.add_research(max_tech_name)
+                did_something = true
+            end
+            if did_something then
+                game.print("Set research to " .. max_tech_name)
+            end
+        end
+        ::continue::
+    end
+end
+
+function create_entity(e)
+    local entity = e.entity
+    if entity == nil or entity.name ~= "research-controller" then
+        return
+    end
+    if storage.research_controllers == nil then
+        storage.research_controllers = {[entity.unit_number] = entity}
+    else
+        storage.research_controllers[entity.unit_number] = entity
+    end
+    script.on_nth_tick(60, update_research)
+end
+
+function destroy_entity(e)
+    local entity = e.entity
+    if not entity or entity.valid or entity.name ~= "research-controller" then
+        return
+    end
+    storage.research_controllers[entity.unit_number] = nil
+    if table_size(global.research_controllers) == 0 then
+        storage.research_controllers = nil
+        script.on_nth_tick(60, nil)
+    end
+    local checker = entity.surface.find_entity("research-controller", entity.position)
+    if checker then
+        checker.destroy({raise_destroy = true})
+    end
+end    
+
+script.on_event(defines.events.on_built_entity, create_entity)
+script.on_event(defines.events.on_entity_cloned, create_entity)
+script.on_event(defines.events.on_robot_built_entity, create_entity)
+script.on_event(defines.events.on_space_platform_built_entity, create_entity)
+script.on_event(defines.events.on_entity_died, destroy_entity)
+script.on_event(defines.events.on_player_mined_entity, destroy_entity)
+script.on_event(defines.events.on_robot_mined_entity, destroy_entity)
+script.on_event(defines.events.on_space_platform_mined_entity, destroy_entity)
+script.on_event(defines.events.on_research_finished, update_research)
+
+script.on_init(function()
+    research_signals.build_signal_name_registry()
+end)
+
+script.on_load(function()
+    research_signals.build_signal_name_registry()
+    if storage.research_controllers then
+        script.on_nth_tick(60, update_research)
+    end
+end)
---
+++ data.lua
@@ -0,0 +1,61 @@
+local constants = require("constants")
+local research_signals = require("research-signals")
+
+research_signals.register_all_technology_signals()
+
+data:extend({{
+  type = "item-subgroup",
+  name = constants.technology_signals_sub_group,
+  group = "signals",
+  order = "t"
+}})
+
+local research_controller = table.deepcopy(data.raw["container"]["steel-chest"])
+research_controller.name = "research-controller"
+research_controller.minable.result = "research-controller"
+research_controller.inventory_size = 0
+research_controller.picture = {
+  layers = {
+    {
+      scale = 0.5,
+      filename = "__Simple_Research_Control_Combinator__/entity-research-controller.png",
+      width = 114,
+      height = 102,
+      shift = util.by_pixel(0, 5)
+    },
+    {
+      scale = 0.5,
+      filename = "__base__/graphics/entity/combinator/constant-combinator-shadow.png",
+      width = 98,
+      height = 66,
+      shift = util.by_pixel(8.5, 5.5),
+      draw_as_shadow = true
+    }
+  }
+}
+
+
+data:extend({research_controller})
+
+data:extend({{
+  type = "item",
+  name = "research-controller",
+  icon = "__Simple_Research_Control_Combinator__/icon-research-controller.png",
+  icon_size = 64,
+  subgroup = "circuit-network",
+  place_result = "research-controller",
+  stack_size = 1,
+  weight = 1000000
+}})
+
+data:extend({{
+  type = "recipe",
+  name = "research-controller",
+  enabled = true,
+  energy_required = 120,
+  ingredients = {
+    {type = "item", name = "quantum-processor", amount = 420},
+    {type = "item", name = "raw-fish", amount = 69}
+  },
+  results = {{type = "item", name = "research-controller", amount = 1}}
+}})
