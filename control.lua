43394574d6b3a7b4526c548616643510bf842520
milkcupatstarbucks <83178133+milkcupatstarbucks@users.noreply.github.com>
2025-08-01 15:06:03 -0700

    optimizations and bugfixes

--- control.lua
+++ control.lua
@@ -1,52 +1,37 @@
 local research_signals = require("research-signals")
 
 function update_research(e)
-    if storage.research_controllers == nil then
+    entity = storage.research_controllers
+    if entity == nil then
         return
     end
-    for id, entity in pairs(storage.research_controllers) do
-        if entity == nil then
-            goto continue
-        end
-        if not entity.valid then
-            goto continue
-        end
-        local behavior = entity.get_or_create_control_behavior()
-        if behavior == nil then
-            goto continue
-        end
-        local signals_red = behavior.get_circuit_network(defines.wire_connector_id.circuit_red)
-        local signals_green = behavior.get_circuit_network(defines.wire_connector_id.circuit_green)
-        local max = 0
-        local max_tech_name = nil
-        for tech, _ in pairs(research_signals.signal_name_registry) do
-            local sig = 0
-            if signals_red then
-                sig = sig + signals_red.get_signal({type="virtual", name=tech})
-            end
-            if signals_green then
-                sig = sig + signals_green.get_signal({type="virtual", name=tech})
-            end
-            if sig > max then
-                max = sig
-                max_tech_name = tech
+    if not entity.valid then
+        storage.research_controllers = nil
+        script.on_nth_tick(60, nil)
+        return
+    end
+    signals = entity.get_signals(defines.wire_connector_id.circuit_red, defines.wire_connector_id.circuit_green)
+    if signals == nil then
+        return
+    end
+    local max = 0
+    local max_tech_name = nil
+    for _, signal in pairs(signals) do
+        if research_signals.signal_name_registry[signal.signal.name] then
+            if signal.count > max then
+                max = signal.count
+                max_tech_name = signal.signal.name
             end
         end
-        if max_tech_name then
-            local did_something = false
-            while entity.force.current_research and entity.force.current_research.name ~= max_tech_name do
-                entity.force.cancel_current_research()
-                did_something = true
-            end
-            if entity.force.current_research == nil then
-                entity.force.add_research(max_tech_name)
-                did_something = true
-            end
-            if did_something then
-                game.print("Set research to " .. max_tech_name)
-            end
+    end
+    if max_tech_name then
+        if entity.force.current_research and entity.force.current_research.name ~= max_tech_name then
+            entity.force.research_queue = nil
         end
-        ::continue::
+        if entity.force.current_research == nil then
+            entity.force.add_research(max_tech_name)
+            game.print("Set research to " .. max_tech_name)
+        end
     end
 end
 
@@ -56,11 +41,15 @@ function create_entity(e)
         return
     end
     if storage.research_controllers == nil then
-        storage.research_controllers = {[entity.unit_number] = entity}
+        storage.research_controllers = entity
+        script.on_nth_tick(60, update_research)
     else
-        storage.research_controllers[entity.unit_number] = entity
+        entity.surface.create_entity{
+            name = "explosion",
+            position = entity.position
+        }
+        entity.destroy()
     end
-    script.on_nth_tick(60, update_research)
 end
 
 function destroy_entity(e)
@@ -68,14 +57,9 @@ function destroy_entity(e)
     if not entity.valid or entity.name ~= "research-controller" then
         return
     end
-    storage.research_controllers[entity.unit_number] = nil
-    if table_size(storage.research_controllers) == 0 then
+    if storage.research_controllers == entity.unit_number then
         storage.research_controllers = nil
         script.on_nth_tick(60, nil)
-    end
-    local checker = entity.surface.find_entity("research-controller", entity.position)
-    if checker then
-        checker.destroy({raise_destroy = true})
     end
 end    
 
@@ -83,14 +67,17 @@ script.on_event(defines.events.on_built_entity, create_entity)
 script.on_event(defines.events.on_entity_cloned, create_entity)
 script.on_event(defines.events.on_robot_built_entity, create_entity)
 script.on_event(defines.events.on_space_platform_built_entity, create_entity)
+script.on_event(defines.events.script_raised_built, create_entity)
 script.on_event(defines.events.on_entity_died, destroy_entity)
 script.on_event(defines.events.on_player_mined_entity, destroy_entity)
 script.on_event(defines.events.on_robot_mined_entity, destroy_entity)
 script.on_event(defines.events.on_space_platform_mined_entity, destroy_entity)
+script.on_event(defines.events.script_raised_destroy, destroy_entity)
 script.on_event(defines.events.on_research_finished, update_research)
 
 script.on_init(function()
     research_signals.build_signal_name_registry()
+    storage.research_controllers = nil
 end)
 
 script.on_load(function()
